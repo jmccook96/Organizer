7f3a1f6d5fb64b40ab870094d89f77075e8cac4f
James McCook <jamesmccook96@gmail.com>
2024-09-17 20:51:32 +1000

    Coloured Navigation Bar
    Implemented basics for re-colouring the navigation bar. Need to change the implementation of MainLayout to boost this functionality rather than doing a shitty hack around.

--- src/main/java/com/bookclub/controller/NavigatorBarController.java
+++ src/main/java/com/bookclub/controller/NavigatorBarController.java
@@ -2,29 +2,66 @@ package com.bookclub.controller;
 
 import com.bookclub.util.StageFactory;
 import com.bookclub.util.StageView;
-
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import java.awt.event.ActionEvent;
 
 public class NavigatorBarController {
-        public void navigateTest() {
-            StageFactory.getInstance().switchScene(StageView.HOME);
+        private Button activeButton;
+
+        // FXML references to buttons
+        @FXML private Button accountButton;
+        @FXML private Button booksButton;
+        @FXML private Button homeButton;
+        @FXML private Button eventsButton;
+        @FXML private Button chatButton;
+
+        public void initialize() {
+            setActiveButton(homeButton);
+        }
+        
+        private void setActiveButton(Button newActiveButton) {
+            if (activeButton != null) {
+                activeButton.setStyle(null);
+            }
+            activeButton = newActiveButton;
+            activeButton.setStyle("-fx-background-color: #00aaff;");
+        }
+        
+        private void navigateView(StageView newView) {
+            if (newView == null) {
+                navigateTest();
+                return;
+            }
+            switch (newView) {
+                case ACCOUNT_SETTINGS -> setActiveButton(accountButton);
+                case BOOKS            -> setActiveButton(booksButton);
+                case HOME             -> setActiveButton(homeButton);
+                case EVENTS           -> setActiveButton(eventsButton);
+            }
+
+            StageFactory.getInstance().switchScene(newView);
+        }
+    
+        private void navigateTest() {
             System.out.println("YET TO BE IMPLEMENTED. GOING HOME.");
+            navigateView(StageView.HOME);
+        }
+
+        public void navigateAccount() {
+            navigateView(StageView.ACCOUNT_SETTINGS);
+        }
+
+        public void navigateBooks() {
+            navigateView(StageView.BOOKS);
         }
     
         public void navigateHome() {
-            StageFactory.getInstance().switchScene(StageView.HOME);
-        }
-        
-        public void navigateBooks() {
-            StageFactory.getInstance().switchScene(StageView.BOOKS);
-        }
-        
-        public void navigateAccount() {
-            StageFactory.getInstance().switchScene(StageView.ACCOUNT_SETTINGS);
+            navigateView(StageView.HOME);
         }
         
         public void navigateEvents() {
-            StageFactory.getInstance().switchScene(StageView.EVENTS);
+            navigateView(StageView.EVENTS);
         }
         
         public void navigateChat() {
