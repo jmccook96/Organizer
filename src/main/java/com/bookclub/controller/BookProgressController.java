e9ede33001c22be70d2064157d9ba8ad09c2b108
James McCook <jamesmccook96@gmail.com>
2024-10-21 16:58:20 +1000

    Import Optimised
    Optimised the imports and enabled building dependency to see what it does.

--- src/main/java/com/bookclub/controller/BookProgressController.java
+++ src/main/java/com/bookclub/controller/BookProgressController.java
@@ -1,7 +1,6 @@
 package com.bookclub.controller;
 
 import com.bookclub.dao.BookProgressDAO;
-import com.bookclub.model.BookProgress;
 import com.bookclub.service.BookProgressService;
 import com.bookclub.service.BookService;
 import com.bookclub.service.LoginService;
